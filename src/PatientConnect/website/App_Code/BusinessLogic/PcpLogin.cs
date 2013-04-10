7a0f69dedb0e567b3cbf0830309c49934edf48ad
Sam Britt <samuelbritt@gmail.com>
2013-04-10 03:08:22 -0400

    initial skeleton of pcp login

---
+++ src/PatientConnect/website/App_Code/BusinessLogic/PcpLogin.cs
@@ -0,0 +1,19 @@
+﻿
+/// <summary>
+/// Summary description for PCPLogin
+/// </summary>
+public class PcpLogin
+{
+	public PcpLogin()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public static bool LoginIsValid(string username, string password)
+    {
+        // TODO
+        return true;
+    }
+}
---
+++ src/PatientConnect/website/pcp.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public partial class pcp : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+    }
+
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        string email = txtUsername.Text;
+        string password = txtPassword.Text;
+        if (PcpLogin.LoginIsValid(email, password))
+        {
+            lblMessage.Text = "Valid Login";
+        }
+        else
+        {
+            lblMessage.Text = "Invalid Login";
+        }
+    }
+}
