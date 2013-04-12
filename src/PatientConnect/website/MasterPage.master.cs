16a7964602554c1c0674648e2df57943c35b37a3
Sam Britt <samuelbritt@gmail.com>
2013-04-12 01:38:00 -0400

    use built-in LoginView controls

--- src/PatientConnect/website/MasterPage.master.cs
+++ src/PatientConnect/website/MasterPage.master.cs
@@ -5,20 +5,5 @@ public partial class MasterPage : System.Web.UI.MasterPage
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        if (!Context.User.Identity.IsAuthenticated)
-        {
-            msgLoginState.Text = "Not Logged In";
-            btnLogout.Visible = false;
-        }
-        else
-        {
-            msgLoginState.Text = Context.User.Identity.Name;
-            btnLogout.Visible = true;
-        }
-    }
-    protected void btnLogout_Click(object sender, EventArgs e)
-    {
-        FormsAuthentication.SignOut();
-        Response.Redirect("login.aspx");
     }
 }
--- src/PatientConnect/website/login.aspx.cs
+++ src/PatientConnect/website/login.aspx.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Web.Security;
+using System.Web.UI;
 
 public partial class login : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Control loginStatus = Master.FindControl("LoginStatus");
+        loginStatus.Visible = false;
     }
 
     protected void btnLogin_Click(object sender, EventArgs e)
